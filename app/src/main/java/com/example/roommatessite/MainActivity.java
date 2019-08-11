ccb7f994fa1fba41f65a629cfd66ada0a667ff36
lishu gupta <lishugupta652@gmail.com>
2019-08-11 22:39:48 +0530

    Main App

---
+++ app/src/main/java/com/example/roommatessite/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.roommatessite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://hotelmanagement-684f5.web.app/");
+
+        webView.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+
+    }
+}
